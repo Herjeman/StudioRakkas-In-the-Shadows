cd6c9c2256069d87da0c34c74e33acbe464575b5
Spirren <107792831+Spirren@users.noreply.github.com>
2023-03-26 21:43:59 +0200

    ui scorinng
    channged locationn of scoriing

--- userinterface.py
+++ userinterface.py
@@ -1,30 +1,30 @@
 import arcade
 import arcade.gui
+import main
 
 class UserInterface:
     def __init__(self):
         self.open_options = False
-        self.points = 0
+        self.points = 100000000000000
 
-
+# arcade.draw_text()
     def recive_key_down(self, key):
         if key == arcade.key.ESCAPE:
             if self.open_options:
                 self.open_options = False
             else:
                 self.open_options = True
-    
     def draw_self(self):
-        arcade.draw_text(self.points,120.0,300.0,
-                         arcade.color.BABY_BLUE,40,80,'left')
+        arcade.draw_text(self.points,main.SCREEN_WIDTH/32, main.SCREEN_HEIGHT/1.10,
+                         arcade.color.BABY_BLUE,40,0,'left')
         if self.open_options:
-            arcade.draw_rectangle_filled(600, 360, 400, 600, arcade.color.RED)
+            arcade.draw_rectangle_filled(main.SCREEN_WIDTH/2, main.SCREEN_HEIGHT/2, main.SCREEN_WIDTH/2, main.SCREEN_HEIGHT/1.25, arcade.color.RED)
                     # Create a vertical BoxGroup to align buttons
-            self.v_box = arcade.gui.UIBoxLayout()
-
-            # Create the buttons
-            start_button = arcade.gui.UIFlatButton(text="Start Game", width=200)
-            self.v_box.add(start_button.with_space_around(bottom=20))
-
-            settings_button = arcade.gui.UIFlatButton(text="Settings", width=200)
-            self.v_box.add(settings_button.with_space_around(bottom=20))
+    
+        # self.uimanager = arcade.gui.UIManager()
+        # self.uimanager.enable()
+        # start_button = arcade.gui.UIFlatButton(text="Start Game",width=200)
+  
+        # start_button.on_click = self.on_buttonclick
+        
+            
