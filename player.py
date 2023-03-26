70f96c1614ed2c4a7b71e74f0d1f91253a000251
Herjeman <englundgustav@gmail.com>
2023-03-26 18:26:41 +0200

    Update player.py

--- player.py
+++ player.py
@@ -4,17 +4,20 @@ from HelperClasses import vector
 class Player:
 
     def __init__(self, spawn_x, spawn_y):
+
         self.up = False
         self.down = False
         self.right = False
         self.left = False
-        self.position = vector.Vector2(spawn_x, spawn_y)
-        self.move = vector.Vector2(0, 0)
 
+        self.position = vector.Vector2(spawn_x, spawn_y)
+
+        self.move = vector.Vector2(0, 0)
+        self.speed = 500
 
     def update(self, delta_time):
 
-        self.position = self.position + self.move
+        self.position = self.position + self.move * self.speed * delta_time
 
     def draw_self(self):
 
@@ -24,34 +27,26 @@ class Player:
 
         if key == arcade.key.W or key == arcade.key.UP:
             self.move.y += 1
-            self.up = True
 
         elif key == arcade.key.S or key == arcade.key.DOWN:
             self.move.y += -1
-            self.down = True
 
         elif key == arcade.key.A or key == arcade.key.LEFT:
             self.move.x += -1
-            self.left = True
 
         elif key == arcade.key.D or key == arcade.key.RIGHT:
             self.move.x += 1
-            self.right = True
 
     def receive_key_up(self, key: int):
 
         if key == arcade.key.W or key == arcade.key.UP:
             self.move.y -= 1
-            self.right = False
 
         elif key == arcade.key.S or key == arcade.key.DOWN:
             self.move.y -= -1
-            self.right = False
 
         elif key == arcade.key.A or key == arcade.key.LEFT:
             self.move.x -= -1
-            self.right = False
 
         elif key == arcade.key.D or key == arcade.key.RIGHT:
             self.move.x -= 1
-            self.right = False
