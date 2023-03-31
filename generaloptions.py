b7d722bc547574dd6d6488b81fa9b2c7632f3fe9
Spirren <107792831+Spirren@users.noreply.github.com>
2023-03-31 13:01:10 +0200

    added sonnf cuntion for disco music
    path still missinng

--- generaloptions.py
+++ generaloptions.py
@@ -122,7 +122,7 @@ class GeneralOptions:
         else:
             main.GAME_MANAGER.disco_mode = True
         print(main.GAME_MANAGER.disco_mode)
-        # self.music_player.play()
+        self.music_player.disco_mode_song()
 
 
     def quit_game_button_click(self, event):
--- musicplayer.py
+++ musicplayer.py
@@ -5,7 +5,9 @@ import main
 class MusicPlayer:
     def __init__(self, window_class):
         song_path = os.path.join("assets", "audio", "in_the_shadows_2.wav")
-        self.song = arcade.load_sound(song_path)
+        disco_song_path = os.path.join("assets", "audio", "in_the_shadows_2.wav")
+        self.normal_song = arcade.load_sound(song_path)
+        self.disco_song = arcade.load_sound(disco_song_path)
         self.pan = 0.0
         self.loop = True
         self.speed = 1.0
@@ -13,6 +15,10 @@ class MusicPlayer:
         self.volume = main.GAME_MANAGER.music_volume
 
         self.active_music_player = None
+        if main.GAME_MANAGER.disco_mode:
+            self.song = self.disco_song
+        else:
+            self.song = self.normal_song
         
     def play(self):
         self.active_music_player = arcade.play_sound(
@@ -39,5 +45,13 @@ class MusicPlayer:
             main.GAME_MANAGER.music_volume -= self.volume_change
             self.song.set_volume(main.GAME_MANAGER.music_volume, self.active_music_player)
 
-    # def disco_mode(self):
-    #     if main.GAME_MANAGER.disco_mode:
+    def disco_mode_song(self):
+        if main.GAME_MANAGER.disco_mode:
+            self.active_music_player.pause()
+            self.song = self.disco_song
+            self.play()
+        else:
+            self.active_music_player.pause()
+            self.song = self.normal_song
+            self.play()
+
